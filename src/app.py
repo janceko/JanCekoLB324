15485f8197b516351296f9cae9d8bb92ccedf2a2
janceko <jan.2003@icloud.com>
2024-10-28 14:35:25 +0100

    dotenv hinzugefügt

--- src/app.py
+++ src/app.py
@@ -1,7 +1,17 @@
+import os
+from dotenv import load_dotenv
 from flask import Flask
 from src.config import Config
 from src.models import db
 from src.routes import init_routes
+
+load_dotenv()
+
+class config:
+    SECRET_KEY = os.getenv('SECRET_KEY', 'standard_geheimer_schluessel')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///tasks.db')
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 
 def create_app():
     app = Flask(__name__)
@@ -11,6 +21,7 @@ def create_app():
         db.create_all()
     init_routes(app)
     return app
+
 
 if __name__ == "__main__":
     app = create_app()
