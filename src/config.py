a25226cf3dc51783da40f12d9a1c51ec44632e21
janceko <jan.2003@icloud.com>
2024-10-28 14:23:42 +0100

    yaml file change and conf

--- src/config.py
+++ src/config.py
@@ -1,9 +1,6 @@
 import os
-import sys
 
 class Config:
-    SQLALCHEMY_DATABASE_URI = os.getenv("DATABASE_URL", "sqlite:///tasks.db")
+    SECRET_KEY = os.getenv('SECRET_KEY', 'supersecretkey')
+    SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL', 'sqlite:///tasks.db')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '../src')))
-from app import create_app
